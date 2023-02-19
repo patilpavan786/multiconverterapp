71298989fc244992c0c02e226acfb0451a63ac4b
patilpavan786 <pavan.p.b.pavan@gmail.com>
2023-02-20 02:31:41 +0530

    copyright added

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <p>&#169; CopyRight by Pavan Patil : frontend Californium  on 2/20/2023</p>
+    </div>
+  )
+}
+
+export default Footer
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,20 +2,24 @@ import React from 'react'
 import style from "./Home.module.css"
 import { useNavigate } from 'react-router-dom'
 import NavBar from '../../Component/NavBar/NavBar'
+import Footer from '../../Component/Footer/Footer'
 function Home() {
   let navigate = useNavigate()
 
   return (
+    <>
     <div className={style.main}>
       <NavBar />
-      <div onClick={()=> navigate("/Area")}><h1>Area converter</h1></div>
-<div onClick={()=> navigate("/Currancy")}><h1>Currancy converter</h1></div>
-<div onClick={()=> navigate("/Length")}><h1>Length converter</h1></div>
-<div onClick={()=> navigate("/Pressure")}><h1>Pressure converter</h1></div>
-<div onClick={()=> navigate("/Volume")}><h1>Volume converter</h1></div>
-<div onClick={()=> navigate("/Weight")}><h1>Weight converter</h1> </div>
-
+      <div onClick={()=> navigate("/Area")} className={style.txt}><h1>Area converter</h1></div>
+<div onClick={()=> navigate("/Currancy")}  className={style.txt2}><h1>Currancy converter</h1></div>
+<div onClick={()=> navigate("/Length")} className={style.txt}><h1>Length converter</h1></div>
+<div onClick={()=> navigate("/Pressure")} className={style.txt2}><h1>Pressure converter</h1></div>
+<div onClick={()=> navigate("/Volume")} className={style.txt}><h1>Volume converter</h1></div>
+<div onClick={()=> navigate("/Weight")} className={style.txt2}><h1>Weight converter</h1> </div>
     </div>
+   <div className={style.Footer}> <Footer  /></div>
+    </>
+
   )
 }
 
